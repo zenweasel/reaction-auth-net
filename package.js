881638c5ec2b8ab8410461f7b72986b92c2f77f4
aaronjudd <aaronjudd@me.com>
2015-07-16 23:13:16 -0700

    updated package version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Reaction Authorize.net - Authorize.net payments for Reaction Commerce",
   name: "reactioncommerce:reaction-auth-net",
-  version: "0.4.0",
+  version: "0.4.2",
   git: "https://github.com/reactioncommerce/reaction-auth-net"
 });
 
@@ -9,10 +9,10 @@ Npm.depends({'paynode': '0.3.6'});
 
 Package.onUse(function (api, where) {
   api.versionsFrom('METEOR@1.0');
-  api.use("meteor-platform@1.2.1");
+  api.use("meteor-platform");
   api.use("coffeescript");
   api.use("less");
-  api.use("reactioncommerce:core@0.5.0");
+  api.use("reactioncommerce:core@0.6.0");
 
   api.addFiles("server/register.coffee",["server"]); // register as a reaction package
   api.addFiles("server/authnet.coffee",["server"]);
